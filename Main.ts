466b16e87b2b16c80dc2536fc7714bf27e478230
ignaciogzlztdf <ignaciogonzalez@tdf.edu.ar>
2022-11-02 00:08:32 -0300

    Escribí los requerimientos del TP, para tenerlos más a mano

---
+++ Main.ts
@@ -0,0 +1,35 @@
+/*
+CASINO
+Generar un programa en cual se puedan realizar
+apuestas dentro de un casino
+
+REQUERIMIENTOS
+1-Tiene un juego tragamonedas que tiene distintas variantes
+siempre cumpliendo una misma funcionalidad, que dependen
+de la temática, del valor de apuesta y de la probabilidad de
+ganar. Deberán hacer 2 variantes (herencia).
+2-Debe tener un mínimo de 2 juegos elegidos por ustedes.
+Cada uno con los valores de apuesta y la probabilidad de
+ganar.
+3-La información de los juegos debe existir en un archivo de
+texto y debe ser leída al momento de iniciar el programa.
+4-El casino deberá proveer funcionalidades para elegir un
+juego y realizar una apuesta, obteniendo el resultado de la
+misma (gano x plata o perdió).
+5-Los resultados de cada juego deberán ser guardados en un
+archivo de texto para fines estadísticos. Cada juego debe
+tener su propio archivo.
+6-Pueden agregar todo lo que consideren necesario para el
+correcto funcionamiento de su programa, siempre que se
+cumplan los requerimientos minimos exigidos
+7-El casino debe ser un proyecto NPM nuevo y se debe
+desarrollar en un branch específico llamado casino-poo.
+8-Debe tener un archivo PDF con una descripción del
+programa y los diagramas de clases correspondientes
+9-Se debe realizar en grupos de MAXIMO 3 integrantes
+(pueden hacerlo de manera individual)
+
+FECHA LÍMITE
+La fecha límite de entrega es el MARTES 8 de noviembre a
+las 23:59
+*/
