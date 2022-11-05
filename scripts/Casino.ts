8b0a39983e9433c4d24165a83d305ea25892ec55
ignaciogzlztdf <ignaciogonzalez@tdf.edu.ar>
2022-11-05 16:46:07 -0300

    Organicé los archivos en carpetas

--- scripts/Casino.ts
+++ scripts/Casino.ts
@@ -2,7 +2,6 @@ import { SlotLuckyBoy } from "./SlotLuckyBoy";
 export class Casino {
   private name:string;
   private slotLuckyboy:SlotLuckyBoy;
-
   // le doy los valores a los juegos con los setters
   public constructor(paramName:string) {
     this.name = paramName;
